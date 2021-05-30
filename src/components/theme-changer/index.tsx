141a9dd4ff551e0450352635ab8b4520e2b26128
subbiah chandramouli <subbiah@logichub.com>
2021-05-30 13:12:11 +0530

    black bg

--- src/components/theme-changer/index.tsx
+++ src/components/theme-changer/index.tsx
@@ -6,16 +6,36 @@ function ThemeChanger(props: { themes: String[] }) {
   const { themes } = props;
   const themesLength = themes.length;
 
-  const [themeIndex, setThemeIndex] = useState(0);
+  const [themeIndex, setThemeIndex] = useState(getLocalstorageTheme);
 
   useMemo(() => {
-    document.body.className = `dh_theme_${themes[themeIndex]}`;
+    const themeClass = `dh_theme_${themes[themeIndex]}`;
+    document.body.className = themeClass;
+    localStorage.setItem("themeClass", themeClass);
   }, [themes, themeIndex]);
 
   function handleThemeChange() {
     const updatedThemeIndex = themeIndex + 1;
 
     setThemeIndex(updatedThemeIndex < themesLength ? updatedThemeIndex : 0);
+  }
+
+  function getLocalstorageTheme() {
+    let themeIndex = 0;
+
+    const themeClass = localStorage.getItem("themeClass");
+
+    if (themeClass) {
+      const themeIndexFound = themes.findIndex((theme) => {
+        return `dh_theme_${theme}` === themeClass;
+      });
+
+      if (themeIndexFound !== -1) {
+        themeIndex = themeIndexFound;
+      }
+    }
+
+    return themeIndex;
   }
 
   return (
